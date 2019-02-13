46c81f5c22cd1684d5c27874c76d9bde901a335c
sam ghoreishi <sam20gh@gmail.com>
2019-02-13 00:25:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,7 +20,7 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	programmer_hash [:alan_kay]
+	
 	
 	programmer_hash = 
  		{
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash [:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash [:dennis_ritchie][:languages]
 end
 
 def adding_matz
