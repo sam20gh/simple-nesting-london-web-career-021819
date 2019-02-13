91c2d18ff5b32de6edcb7dd0bb99db5344cc4979
sam ghoreishi <sam20gh@gmail.com>
2019-02-13 00:30:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,6 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-   programmer_hash[:dennis_ritchie][:languages] = "alans_new_info"
+   programmer_hash[:dennis_ritchie][:languages] = ""
      programmer_hash
 end
